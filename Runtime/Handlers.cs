02d95cdf45e429af1eeca7ceb22add76cc92ee5c
Dastan Dzhumabekov <dastan.wm@gmail.com>
2022-09-07 16:31:56 +0600

    Handler refactor

--- Runtime/Handlers.cs
+++ Runtime/Handlers.cs
@@ -2,6 +2,6 @@
 
 namespace Dythervin.Callbacks
 {
-    public delegate void TriggerHandler(ITriggerCallbacks callbacks, Collider collider);
-    public delegate void TriggerStayHandler(ITriggerStayCallback callbacks, Collider collider);
+    public delegate void TriggerHandler(ITriggerCallbacks callbacks, Collider other);
+    public delegate void TriggerStayHandler(ITriggerStayCallback callbacks, Collider other);
 }
